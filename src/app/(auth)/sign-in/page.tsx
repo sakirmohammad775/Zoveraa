10717b962841c086fb2d23db81077ccacfbfe7c9
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2026-06-24 02:00:11 +0600

    Authentication socials-github and google

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,11 +1,20 @@
+import { auth } from "@/lib/auth";
 import { SignInView } from "@/modules/auth/ui/views/sign-in-view";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-const page = () => {
-    return ( 
-        <>
-        <SignInView></SignInView>
-        </>
-     );
-}
- 
+const page = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!!session) {
+    redirect("/");
+  }
+  return (
+    <>
+      <SignInView></SignInView>
+    </>
+  );
+};
+
 export default page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,64 +1,18 @@
-"use client";
-import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
+import { auth } from "@/lib/auth";
+import {HomeView} from "@/modules/home/ui/views/home-view";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-import { Input } from "@base-ui/react";
-import { useState } from "react";
 
-export default function Home() {
-  const { data: session } = authClient.useSession();
-
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const onSubmit = () => {
-    authClient.signUp.email(
-      {
-        email,
-        name,
-        password,
-      },
-      {
-        onError: () => {
-          window.alert("SOmething went wrong");
-        },
-        onSuccess: () => {
-          window.alert("Success");
-        },
-      },
-    );
-  };
-  if (session){
-    return(
-      <div>
-        <p>Logged i as {session.user.name}</p>
-        <Button onClick={()=>authClient.signOut()}>Sign out</Button>
-      </div>
-    )
+const Page = async () => {
+  const session =await auth.api.getSession({
+    headers:await headers()
+  })
+  if(!session){
+    redirect("/sign-in")
   }
-
-  return (
-    <>
-      <div>
-        <Input
-          placeholder="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        ></Input>
-        <Input
-          placeholder="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        ></Input>
-        <Input
-          placeholder="password"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        ></Input>
-        <Button onClick={onSubmit}>create user</Button>
-      </div>
-    </>
-  );
+  return <HomeView></HomeView>
+  
 }
+ 
+export default Page;
