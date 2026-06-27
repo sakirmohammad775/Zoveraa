b1989ac29c033d962666e86b16f87bcb4f29f81a
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2026-06-27 10:46:36 +0600

    19-meetings-filter

--- src/app/(dashboard))/meetings/page.tsx
+++ src/app/(dashboard))/meetings/page.tsx
@@ -1,26 +1,50 @@
 import { Suspense } from "react";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 import { ErrorBoundary } from "react-error-boundary";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
+import { auth } from "@/lib/auth";
 import { getQueryClient, trpc } from "@/trpc/server";
-import { MeetingsView, MeetingsViewError, MeetingsViewLoading } from "@/modules/meetings/ui/views/meeting-view";
 
+import { 
+  MeetingIdView, 
+  MeetingIdViewError, 
+  MeetingIdViewLoading
+} from "@/modules/meetings/ui/views/meeting-id-view";
 
-const Page = () => {
+interface Props {
+  params: Promise<{
+    meetingId: string;
+  }>;
+}
+
+const Page = async ({ params }: Props) => {
+  const { meetingId } = await params;
+
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    redirect("/sign-in");
+  }
+
   const queryClient = getQueryClient();
   void queryClient.prefetchQuery(
-    trpc.meetings.getMany.queryOptions({})
+    trpc.meetings.getOne.queryOptions({ id: meetingId }),
   );
+  // TODO: Prefetch `meetings.getTranscript`
 
-  return (
+  return ( 
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<MeetingsViewLoading />}>
-        <ErrorBoundary fallback={<MeetingsViewError />}>
-          <MeetingsView />
+      <Suspense fallback={<MeetingIdViewLoading />}>
+        <ErrorBoundary fallback={<MeetingIdViewError />}>
+          <MeetingIdView meetingId={meetingId} />
         </ErrorBoundary>
       </Suspense>
     </HydrationBoundary>
-  );
-};
+   );
+}
  
 export default Page;
--- src/modules/meetings/ui/views/meeting-view.tsx
+++ src/modules/meetings/ui/views/meeting-view.tsx
@@ -1,22 +1,40 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { useSuspenseQuery } from "@tanstack/react-query";
 
 import { useTRPC } from "@/trpc/client";
+
 import { EmptyState } from "@/components/empty-state";
 import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
+import { DataPagination } from "@/components/data-pagination";
 
 import { columns } from "../components/columns";
+import { useMeetingsFilters } from "../../hooks/use-meetings-filters";
 import { DataTable } from "@/modules/agents/ui/components/data-table";
 
 export const MeetingsView = () => {
   const trpc = useTRPC();
-  const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}));
+  const router = useRouter();
+  const [filters, setFilters] = useMeetingsFilters();
+
+  const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({
+    ...filters,
+  }));
   
   return (
     <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
-      <DataTable data={data.items} columns={columns} />
+      <DataTable 
+        data={data.items} 
+        columns={columns} 
+        onRowClick={(row) => router.push(`/meetings/${row.id}`)}
+      />
+      <DataPagination
+        page={filters.page}
+        totalPages={data.totalPages}
+        onPageChange={(page) => setFilters({ page })}
+      />
       {data.items.length === 0 && (
         <EmptyState
           title="Create your first meeting"
